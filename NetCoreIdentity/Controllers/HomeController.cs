32b9622ea3020d5c1d90982eebd3f398770ab3ef
Emad Khan <emadkhanqai@gmail.com>
2023-08-01 22:53:57 +0500

    first commit with git ignore

---
+++ NetCoreIdentity/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace NetCoreIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Emad"),
+                new Claim(ClaimTypes.Email,"Emadkhanqai@gmail.com"),
+                new Claim("Grandma.says","Very Nice"),
+            };
+
+            // verify
+
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Danish"),
+                new Claim("DrivigLicense","A+"),
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "License Identity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
